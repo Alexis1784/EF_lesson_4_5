2d4266d052e4ed571b6745a73c73571e81da156e
Alex <alexis1784@gmail.com>
2016-01-25 16:43:46 +0500

    have added groupby method

--- EF_lesson_4_3/Program.cs
+++ EF_lesson_4_3/Program.cs
@@ -12,9 +12,15 @@ namespace EF_lesson_4_3
         {
             using (PhoneContext db = new PhoneContext())
             {
-                var phones = db.Phones.Where(p => p.Company.Name == "Samsung");
-                foreach (Phone p in phones)
-                    Console.WriteLine("{0}.{1} - {2}", p.Id, p.Name, p.Price);
+                var groups = from p in db.Phones
+                             group p by p.Company.Name;
+                foreach (var g in groups)
+                {
+                    Console.WriteLine(g.Key);
+                    foreach (var p in g)
+                        Console.WriteLine("{0} - {1}", p.Name, p.Price);
+                    Console.WriteLine();
+                }
             }
             Console.ReadLine();
         }
